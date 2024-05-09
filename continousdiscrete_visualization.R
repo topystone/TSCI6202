e333dbd3af894d60d3e73893ec225df8ac536041
Yun <shiy@uthscsa.edu>
2024-05-09 14:48:33 -0500

    class 5.9.24

--- continousdiscrete_visualization.R
+++ continousdiscrete_visualization.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(rio)
 library(dplyr)
 library(ggsci)
+library(GGally)
 
 inputdata<-"syndata_df.csv"
 if (!file.exists(inputdata)){system("R -f datasynthesis.R")}
@@ -20,3 +21,19 @@ subset(d0,!is.na(bpd_or_death)) %>%
   ggplot(aes(x=factor(bpd_or_death),color=factor(bpd_or_death),y=bw))+geom_violin(fill="green",linewidth=2)+
   geom_boxplot(width = 0.1,color="red",fill="purple",alpha=0.5)+coord_flip()
 
+ggpairs(iris,aes(col=Species))
+
+psych_variables <- attr(psychademic, "psychology")
+academic_variables <- attr(psychademic, "academic")
+
+ggduo(psychademic,
+  mapping = aes(color = sex),
+  academic_variables, psych_variables)
+
+data(psychademic)
+
+independent_variables<-c("ga","bw","gender","average72hr")
+dependent_variables<-c("vent_days","bpd_or_death")
+
+mutate(d0,gender=factor(gender),bpd_or_death=ordered(bpd_or_death)) %>%
+  ggduo(mapping=aes(color = gender),independent_variables,dependent_variables)
--- ecomap.R
+++ ecomap.R
@@ -2,26 +2,40 @@ library(kinship2)
 library(dplyr)
 library(randomNames)
 library(plotrix)
+library(colourpicker)
 
+data("sample.ped")
 ecomaps<-mutate(sample.ped,
        varx=sample(0:1,n(),rep=TRUE),
        age=sample(0:99,n(),rep=TRUE),
        name=randomNames(n(),which.names = "first",gender=sex-1,
-                        sample.with.replacement = FALSE) %>% paste0(.,"\n",age),
+                        sample.with.replacement = FALSE) %>% paste0("\n\n",.,"\n",age),
        fathername=sapply(father,function(xx) {coalesce(name[match(xx,id)],"0")}),
        mothername=sapply(mother,function(xx) {coalesce(name[match(xx,id)],"0")}),
-       overplotcir=sample(c(FALSE,TRUE),n(),prob =c(0.9,0.1),rep=TRUE)
+       overplot = case_match(sex,
+                             1 ~ sample(c('none','square'),n(),prob=c(0.5,0.5),rep=T),
+                             2 ~ sample(c('none','circle'),n(),prob=c(0.5,0.5),rep=T),
+                             T ~ 'none'
        )
+)
 
 ecomaps.ped<-with(ecomaps,pedigree(id=id, dadid=father, momid=mother,sex=sex,
                                    affected=cbind(affected, avail,varx),
                                    famid=ped))
 
 ecomaps.ped$id<-ecomaps$name
-ecomaps.plotdata<-plot(ecomaps.ped[2])
+oldpar<-par()
+par(srt=180,crt=180)
+ecomaps.plotdata<-plot(ecomaps.ped[2],cex=0.6)
 
-with(ecomaps.plotdata,mapply(draw.circle,x[subset(ecomaps,ped==2)$overplotcir],
-                                  y[subset(ecomaps,ped==2)$overplotcir]+boxh/2,
-                                  MoreArgs=list(lwd=2,border="red",radius = boxh/2)))
+with(ecomaps.plotdata,mapply(draw.circle,x[subset(ecomaps,ped==2)$overplot=='circle'],
+                             y[subset(ecomaps,ped==2)$overplot=='circle']+boxh/2,
+                             MoreArgs=list(lwd=2,border="red",radius = boxw/2)))
 
-# rect, boxh, boxw
+with(ecomaps.plotdata,rect(xleft=x[which(subset(ecomaps,ped==2)$overplot=='square')]-boxw/2,
+                           ybottom=y[which(subset(ecomaps,ped==2)$overplot=='square')],
+                           xright=x[which(subset(ecomaps,ped==2)$overplot=='square')]+boxw/2,
+                           ytop=y[which(subset(ecomaps,ped==2)$overplot=='square')]+boxh,
+                           border='blue',col="#BDD9346c",lwd=2))
+
+with(ecomaps.plotdata,text(x=x[4],y=y[4]+boxh/2,"diseased",col="purple"))
